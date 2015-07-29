561dc85b4022694afe66e56536ea265934da2814
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-07-29 01:18:38 +0000

    change invoice model

--- api/models/Invoice.js
+++ api/models/Invoice.js
@@ -8,7 +8,83 @@
 module.exports = {
 
   attributes: {
-
+      // required fields
+      price:{
+        type:"number", 
+        required:true,
+      },
+      currency:{
+        type:"string",
+        required: true,
+        defaultsTo: "PKR",
+        enum: ["PKR", "USD", "BTC"]
+      },
+      
+      // item description
+      itemDesc:{
+        type:"string",
+        size:"100"
+      },
+      
+      //  notification settings
+      notificationUrl: {
+          type:"string"
+      },
+      redirectUrl:{
+          type:"string"
+      },
+      
+      // conformations
+      
+      conformationSpeed:{
+          type:"number"
+      },
+      
+      //others 
+      
+      physical:{
+          type:"boolean",
+          defaultsTo:"false"
+      },
+     
+     // buyer details
+     
+     buyerName:{
+        type:"string",
+     },
+     buyerAddress:{
+        type:"string", 
+     },
+     buyerLocality:{
+        type:"string", 
+     },
+     buyerRegion:{
+        type:"string", 
+     },
+     buyerPostelCode:{
+        type:"number", 
+     },
+     buyerCountry:{
+        type:"string" 
+     },
+     buyerEmail:{
+        type: 'string',
+        email: true
+     },
+     buyerPhone:{
+        type:"number" 
+     },
+     
+     // status of invoice i.e confirmed 
+     status:{
+        type:"string" 
+     },
+     
+     // exception for paidOver or paidPartial , or false
+     
+     exception:{
+        type:"boolean"
+     }
   }
 };
 
