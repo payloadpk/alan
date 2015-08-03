00706f269059952a6fcc5bb0d05cc00139a9ff9c
Waleed Ashraf <waleedashraf@outlook.com>
2015-08-03 11:29:40 +0500

    Uncomment
    redis

--- config/redis.js
+++ config/redis.js
@@ -1,9 +1,9 @@
-// var redis = require('redis');
-// var url = require('url');
+var redis = require('redis');
+var url = require('url');
 
-// var redisURL = url.parse(process.env.REDIS_URL);
-// var client = redis.createClient(redisURL.port, redisURL.hostname);
-// client.auth(redisURL.auth.split(":")[1]);
+var redisURL = url.parse(process.env.REDIS_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname);
+client.auth(redisURL.auth.split(":")[1]);
 
 
-// module.exports.redis = client;
+module.exports.redis = client;
