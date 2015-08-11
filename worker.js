0cd405e69a93571f4c3c10bd7ef55468aba2555a
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-08-12 02:05:07 +0500

    instantiate sails.load

--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
-var market = require('./worker/market.js');
-
-
-// push continuous rate updates to redis
-market.contRate();
+var Sails = require('sails');
+Sails.load(function(err, sails) {
+  var market = require('./worker/market.js');
+  // push continuous rate updates to redis
+  market.contRate();
+});
